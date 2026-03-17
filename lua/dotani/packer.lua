8137905cab75e33e7f372e2b84ce2ae0a4f6ae5f
doten <thedarthdot@gmail.com>
2026-03-17 14:14:11 -0500

    fix treesitter

--- lua/dotani/packer.lua
+++ lua/dotani/packer.lua
@@ -5,78 +5,78 @@ vim.cmd [[packadd packer.nvim]]
 
 
 return require('packer').startup(function(use)
-	-- Packer can manage itself
-	use 'wbthomason/packer.nvim'
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
 
 
-	use {
-		'nvim-telescope/telescope.nvim',
-		branch = 'master', -- Switched from tag/0.1.x to master
-		requires = { 
-			{'nvim-lua/plenary.nvim'}, 
-			{'debugloop/telescope-undo.nvim'} 
-		}
-	}	-- Or with configuration
-	use({
-		'projekt0n/github-nvim-theme',
-		config = function()
-			require('github-theme').setup({
-				-- ...
-			})
+  use {
+    'nvim-telescope/telescope.nvim',
+    branch = 'master', -- Switched from tag/0.1.x to master
+    requires = { 
+      {'nvim-lua/plenary.nvim'}, 
+      {'debugloop/telescope-undo.nvim'} 
+    }
+  }	-- Or with configuration
+  use({
+    'projekt0n/github-nvim-theme',
+    config = function()
+      require('github-theme').setup({
+        -- ...
+      })
 
-			vim.cmd('colorscheme github_dark_default')
-		end
-	})
+      vim.cmd('colorscheme github_dark_default')
+    end
+  })
 
-	--treesitter
-	use {
-		'nvim-treesitter/nvim-treesitter',
-		run = function()
-			local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
-			ts_update()
-		end,
-	}
+  --treesitter
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = function()
+      local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+      ts_update()
+    end,
+  }
 
-	use('ThePrimeagen/harpoon')
-	use('tpope/vim-fugitive')
-	use('neovim/nvim-lspconfig')
-	use {
-		'nvim-lualine/lualine.nvim',
-		requires = { 'nvim-tree/nvim-web-devicons', opt = true }
-	}
-	use('VidocqH/lsp-lens.nvim')
-	use({
-		"https://git.sr.ht/~whynothugo/lsp_lines.nvim",
-		config = function()
-			require("lsp_lines").setup()
-		end,
-	})
-
-
+  use('ThePrimeagen/harpoon')
+  use('tpope/vim-fugitive')
+  use('neovim/nvim-lspconfig')
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+  }
+  use('VidocqH/lsp-lens.nvim')
+  use({
+    "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+    config = function()
+      require("lsp_lines").setup()
+    end,
+  })
 
 
-	-- Autocompletion plugins
-	use 'hrsh7th/nvim-cmp'          -- Completion engine
-	use 'hrsh7th/cmp-nvim-lsp'      -- LSP source for nvim-cmp
-	use 'hrsh7th/cmp-buffer'        -- Buffer completions
-	use 'hrsh7th/cmp-path'          -- Path completions
-	use 'hrsh7th/cmp-cmdline'       -- Cmdline completions
-	use 'L3MON4D3/LuaSnip'          -- Snippet engine (optional but recommended)
-	use 'saadparwaiz1/cmp_luasnip'  -- Snippet completions (optional)
 
-	use {
-		"folke/trouble.nvim",
-		requires = "nvim-tree/nvim-web-devicons",
-		config = function()
-			require("trouble").setup {}
-		end
-	}
 
-	use {
-		"pmizio/typescript-tools.nvim",
-		requires = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-		config = function()
-			require("typescript-tools").setup {}
-		end,
-	}
+  -- Autocompletion plugins
+  use 'hrsh7th/nvim-cmp'          -- Completion engine
+  use 'hrsh7th/cmp-nvim-lsp'      -- LSP source for nvim-cmp
+  use 'hrsh7th/cmp-buffer'        -- Buffer completions
+  use 'hrsh7th/cmp-path'          -- Path completions
+  use 'hrsh7th/cmp-cmdline'       -- Cmdline completions
+  use 'L3MON4D3/LuaSnip'          -- Snippet engine (optional but recommended)
+  use 'saadparwaiz1/cmp_luasnip'  -- Snippet completions (optional)
+
+  use {
+    "folke/trouble.nvim",
+    requires = "nvim-tree/nvim-web-devicons",
+    config = function()
+      require("trouble").setup {}
+    end
+  }
+
+  use {
+    "pmizio/typescript-tools.nvim",
+    requires = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+    config = function()
+      require("typescript-tools").setup {}
+    end,
+  }
 end)
